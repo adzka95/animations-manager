24276a90279f8c0d33f386401cb4158a5ca5ddb1
Herostrates94 <p.birgiel@gmail.com>
2018-05-07 22:54:12 +0200

    [Pawel] Initial commit. Working solution (except custom animations)

---
+++ app/src/main/java/friendlyapps/animationsloader/categorylist/ListAdapterPictures.java
@@ -0,0 +1,144 @@
+package friendlyapps.animationsloader.categorylist;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+import java.util.List;
+
+import friendlyapps.animationsloader.R;
+import friendlyapps.animationsloader.api.entities.Picture;
+import friendlyapps.animationsloader.api.entities.PicturesContainer;
+import friendlyapps.animationsloader.api.managers.StorageAnimationsManager;
+import friendlyapps.animationsloader.api.managers.DatabaseHelper;
+
+public class ListAdapterPictures extends ArrayAdapter<Picture> {
+
+    DatabaseHelper databaseHelper;
+    List<Picture> items;
+    PicturesContainer picturesContainer;
+
+    public ListAdapterPictures(Context context, int textViewResourceId) {
+        super(context, textViewResourceId);
+
+        databaseHelper = new DatabaseHelper(context);
+    }
+
+    public ListAdapterPictures(Context context, int resource, List<Picture> items, PicturesContainer picturesContainer) {
+        super(context, resource, items);
+        this.items = items;
+        databaseHelper = new DatabaseHelper(context);
+        this.picturesContainer = picturesContainer;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v = convertView;
+
+        if (v == null) {
+            LayoutInflater vi;
+            vi = LayoutInflater.from(getContext());
+            v = vi.inflate(R.layout.picturerow, null);
+        }
+
+        final Picture picture = getItem(position);
+
+        if (picture != null) {
+
+            final CheckBox tt2 = v.findViewById(R.id.isEnabled);
+            final ImageButton deleteButton = v.findViewById(R.id.delete_btn);
+            final ImageView imageView = v.findViewById(R.id.imageView);
+
+            if (tt2 != null) {
+
+                if(picture.getEnabled() == 1){
+                    tt2.setChecked(true);
+                }
+                else{
+                    tt2.setChecked(false);
+                }
+
+            }
+
+            tt2.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+
+                    if(tt2.isChecked()){
+                        picture.setEnabled(1);
+                    }
+                    else{
+                        picture.setEnabled(0);
+                    }
+
+                    try {
+                        databaseHelper.getPictureDao().update(picture);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+
+            deleteButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+
+                    try {
+                        StorageAnimationsManager.getInstance().deletePictureFromStorage(picture);
+                        databaseHelper.getPictureDao().delete(picture);
+                        items.remove(picture);
+                        picturesContainer.getPicturesInCategory().remove(picture);
+                        notifyDataSetChanged();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            loadPictureToImageView(imageView, picture);
+
+
+        }
+
+        return v;
+    }
+
+    private void loadPictureToImageView(ImageView imageView, Picture picture){
+
+        File imgFile = new  File(picture.getPath());
+
+        if(imgFile.exists()){
+
+            try {
+                FileInputStream fileInputStream = new FileInputStream(imgFile);
+                Bitmap myBitmap = BitmapFactory.decodeStream(fileInputStream);
+                Bitmap resized = Bitmap.createScaledBitmap(myBitmap, myBitmap.getWidth()*2, myBitmap.getHeight()*2, true);
+                imageView.setImageBitmap(resized);
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+
+            Log.i("Files", "Picture loaded from path: " + picture.getPath());
+
+        }
+    }
+
+
+}
---
+++ app/src/main/java/friendlyapps/animationsloader/categorylist/ListAdapterPicturesContainers.java
@@ -0,0 +1,160 @@
+package friendlyapps.animationsloader.categorylist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import friendlyapps.animationsloader.MainActivity;
+import friendlyapps.animationsloader.R;
+import friendlyapps.animationsloader.api.entities.Picture;
+import friendlyapps.animationsloader.api.entities.PicturesContainer;
+import friendlyapps.animationsloader.api.managers.StorageAnimationsManager;
+import friendlyapps.animationsloader.api.managers.DatabaseHelper;
+
+public class ListAdapterPicturesContainers extends ArrayAdapter<PicturesContainer> {
+
+    DatabaseHelper databaseHelper;
+    List<PicturesContainer> items;
+
+    public ListAdapterPicturesContainers(Context context, int textViewResourceId) {
+        super(context, textViewResourceId);
+
+        databaseHelper = new DatabaseHelper(context);
+    }
+
+    public ListAdapterPicturesContainers(Context context, int resource, List<PicturesContainer> items) {
+        super(context, resource, items);
+        this.items = items;
+        databaseHelper = new DatabaseHelper(context);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v = convertView;
+
+        if (v == null) {
+            LayoutInflater vi;
+            vi = LayoutInflater.from(getContext());
+            v = vi.inflate(R.layout.categoryrow, null);
+        }
+
+        final PicturesContainer picturesContainer = getItem(position);
+
+        if (picturesContainer != null) {
+            TextView tt1 = v.findViewById(R.id.categoryName);
+            final CheckBox tt2 = v.findViewById(R.id.isEnabled);
+            final ImageButton deleteButton = v.findViewById(R.id.delete_btn);
+            final ImageButton editButton = v.findViewById(R.id.edit_btn);
+
+            if (tt1 != null) {
+                tt1.setText(picturesContainer.getCategoryName());
+            }
+
+            if (tt2 != null) {
+
+                if(picturesContainer.getEnabled() == 1){
+                    tt2.setChecked(true);
+                }
+                else{
+                    tt2.setChecked(false);
+                }
+
+            }
+
+            tt2.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+
+                    if(tt2.isChecked()){
+                        picturesContainer.setEnabled(1);
+                    }
+                    else{
+                        picturesContainer.setEnabled(0);
+                    }
+
+                    try {
+                        databaseHelper.getPictureContainerDao().update(picturesContainer);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+
+            deleteButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+
+                    try {
+                        StorageAnimationsManager.getInstance().deletePicturesContainerFromStorage(picturesContainer);
+
+                        for(Picture picture : picturesContainer.getPicturesInCategory()){
+                            databaseHelper.getPictureDao().delete(picture);
+                        }
+
+                        databaseHelper.getPictureContainerDao().delete(picturesContainer);
+                        items.remove(picturesContainer);
+                        notifyDataSetChanged();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            editButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+
+                    loadPicturesToRightPanel(picturesContainer);
+
+                }
+            });
+        }
+
+        return v;
+    }
+
+    private void loadPicturesToRightPanel(PicturesContainer picturesContainer){
+
+        MainActivity mainActivity = (MainActivity) getContext();
+
+        ListView yourListView = mainActivity.findViewById(R.id.newList);
+
+        // get data from the table by the ListAdapter
+        ListAdapterPictures customAdapter =
+                new ListAdapterPictures(getContext(), R.layout.picturerow, new ArrayList<>(picturesContainer.getPicturesInCategory()), picturesContainer);
+
+        yourListView.setAdapter(customAdapter);
+        mainActivity.setCurrentPicturesContainer(picturesContainer);
+        prepareAnimationTypesCheckBoxes(mainActivity);
+
+    }
+
+    void prepareAnimationTypesCheckBoxes(MainActivity mainActivity){
+
+        final CheckBox checkBoxLEFT_TO_RIGHT = mainActivity.findViewById(R.id.checkBoxLEFT_TO_RIGHT);
+        final CheckBox checkBoxSPIRAL = mainActivity.findViewById(R.id.checkBoxSPIRAL);
+        final CheckBox checkBoxUP_DOWN = mainActivity.findViewById(R.id.checkBoxUP_DOWN);
+
+        checkBoxLEFT_TO_RIGHT.setVisibility(View.VISIBLE);
+        checkBoxSPIRAL.setVisibility(View.VISIBLE);
+        checkBoxUP_DOWN.setVisibility(View.VISIBLE);
+
+        checkBoxLEFT_TO_RIGHT.setChecked(mainActivity.getCurrentPicturesContainer().getAnimationTypes().contains("LEFT_TO_RIGHT"));
+        checkBoxSPIRAL.setChecked(mainActivity.getCurrentPicturesContainer().getAnimationTypes().contains("SPIRAL"));
+        checkBoxUP_DOWN.setChecked(mainActivity.getCurrentPicturesContainer().getAnimationTypes().contains("UP_DOWN"));
+
+    }
+
+}
